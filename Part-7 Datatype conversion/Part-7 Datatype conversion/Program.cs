0443f52653c16ab675835fb65f5da281cf07473a
uges.ske <uges.ske@gmail.com>
2018-04-08 08:59:56 +0545

    Completed Practice of parse tryparse along with implicit and explicit conversion

--- Part-7 Datatype conversion/Part-7 Datatype conversion/Program.cs
+++ Part-7 Datatype conversion/Part-7 Datatype conversion/Program.cs
@@ -13,7 +13,29 @@ namespace Part_7_Datatype_conversion
             int i = 100;
             float j = i;
             Console.WriteLine("The value of float is ", j);
+
+            //testing for float
+            float a = 10.223F;
+            int b = Convert.ToInt32(a);
+            Console.WriteLine("this is float value {0}",b);
+
+            //explicit conversion for string
+            string num = "110DB";
+            //int c = int.Parse(num);
+            //incase of the incorrect formate we use tryparse
+            int result;
+            bool isConversionSuccessful = int.TryParse(num, out result);
+            if (isConversionSuccessful)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
             Console.ReadKey();
+
+
         }
     }
 }
