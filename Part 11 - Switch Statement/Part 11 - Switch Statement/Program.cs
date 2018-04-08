23509a374af430a2f8a7d6c9973ebc9dee3581bd
uges.ske <uges.ske@gmail.com>
2018-04-08 13:58:50 +0545

    Studying Switch Statement completed

---
+++ Part 11 - Switch Statement/Part 11 - Switch Statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_11___Switch_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number");
+            int userNumber = int.Parse(Console.ReadLine());
+            switch (userNumber)
+            {
+                //binding all the cases
+                case 10:
+                case 20:
+                case 30:
+                    Console.WriteLine("User input is {0}", userNumber);
+                    break;
+                default:
+                    Console.WriteLine("No any case matches");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
